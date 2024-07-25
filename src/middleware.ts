0e6c27852e39d11a01f78140b8746ae610fb9cd4
indunil-jay <indunil.jayz@gmail.com>
2024-07-25 07:35:32 +0530

    build next-auth middleware for give diferebt access to the diferent rooutes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,47 @@
 import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
+import {
+  apiAuthPrefix,
+  DEFAULT_LOGIN_REDIRECT,
+  protectedRoutes,
+  publicRoutes,
+} from "@/routes";
 
 const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
-  console.log(!!req.auth);
-  console.log("Route :", req.nextUrl.pathname);
+  const { nextUrl } = req;
+  //1. check logging status ture or false
+  const isLoggedIn = !!req.auth;
+
+  //2.use routes
+  //always need to be open becuase user has some api loggin methods
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  //publically accessible routes in app
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  //protected resource, login is required for access
+  const isProtectedRoute = protectedRoutes.includes(nextUrl.pathname);
+
+  //3.actions needs to be done for each type of routes
+  //simply give permission
+  if (isApiAuthRoute) return;
+
+  //
+  if (isProtectedRoute) {
+    // protected and user already logged in the redirect
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl)); //here nextUrl is need for build absolute path url
+    }
+    //otherwise restrict
+    return;
+  }
+
+  //
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+
+  return;
 });
 
 //matcher that runner in middleware
---
+++ src/routes.ts
@@ -0,0 +1,29 @@
+/**
+ * An Array of routes that are accessible to the public
+ * Theser routes do not require authentication
+ * @type {string[]}
+ */
+
+export const publicRoutes: string[] = ["/"];
+
+/**
+ * An Array of routes that can be used after authentication
+ * Theser routes require authentication
+ * @type {string[]}
+ */
+
+export const protectedRoutes: string[] = ["/auth/login", "/auth/register"];
+
+/**
+ * The prefix for API authentication routes
+ * Routes that start with this prefix will be used api authentication purpose
+ * @type {string}
+ */
+
+export const apiAuthPrefix: string = "/api/auth";
+
+/**
+ * The defaul redirect after login
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT: string = "/settings";
