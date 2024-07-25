fddea6e4764f8964bdecd36085edf8099f17cd47
indunil-jay <indunil.jayz@gmail.com>
2024-07-25 05:53:48 +0530

    register user in db using credential

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,7 +1,9 @@
 "use server";
-
+import bcrypt from "bcryptjs";
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   //server-side validation
@@ -11,5 +13,27 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Invalid fields!" };
   }
 
-  return { success: "Email sent" };
+  const { password, email, name } = validatedFields.data;
+
+  //1.check email already exists
+  // const existingUser = await db.user.findUnique({
+  //   where: { email },
+  // });
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use" };
+  }
+
+  //2.hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  //3 create user
+  await db.user.create({
+    data: { email, password: hashedPassword, name },
+  });
+
+  //4 TODO: send verification token
+
+  return { success: "User created" };
 };
